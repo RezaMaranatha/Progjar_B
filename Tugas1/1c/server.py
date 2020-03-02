e287789fbd1285d1ef3cced4ba14098b4d02332d
RezaMaranatha <rezamaranatha@yahoo.com>
2020-03-02 22:38:13 +0700

    update server ip

--- Tugas1/1c/server.py
+++ Tugas1/1c/server.py
@@ -4,8 +4,7 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port_num = int(input("Input Port Number: "))
 # Bind the socket to the port
-host_name = socket.gethostname() 
-host_ip = socket.gethostbyname(host_name)
+host_ip = '192.168.0.13'
 server_address = (host_ip, port_num)
 print("Starting Server on", host_ip, " Port:", port_num)
 sock.bind(server_address)
